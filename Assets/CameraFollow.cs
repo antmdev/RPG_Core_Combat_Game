a2ff24f90d17599d1d631bea815d227c0e35e9da
antmdev <executivesales@hotmail.co.uk>
2019-03-01 14:59:31 +0000

    initial Commit - character moving wasd

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player; 
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player"); 
+		// print(player.ToString()); //check players recognised
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position; //fix camera tranfrom to position of player
+	}
+}
