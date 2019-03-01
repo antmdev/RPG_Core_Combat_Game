88c3aaf321558869db7a4524ca5f1cf784393d3a
antmdev <executivesales@hotmail.co.uk>
2019-03-01 16:42:11 +0000

    implemented click to move

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+
+	[SerializeField] float walkMoveStopRadius = 0.2f;
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
@@ -21,10 +23,32 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
-            currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
-        }
-        m_Character.Move(currentClickTarget - transform.position, false, false);
-    }
+            print("Cursor raycast hit layer " + cameraRaycaster.layerHit);
+
+            switch(cameraRaycaster.layerHit)
+            {
+           		case Layer.Walkable:
+					currentClickTarget = cameraRaycaster.hit.point; 
+					break;
+				case Layer.Enemy:
+					print("Not moving to enemy");
+					break;
+				default:
+					print("Unexpected layer found");
+					return;
+
+        	}
+
+    	}
+		var playerToClickPoint = currentClickTarget - transform.position;
+    	if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+    	{
+			m_Character.Move(currentClickTarget - transform.position, false, false);
+		}
+		else
+		{
+		 	m_Character.Move(Vector3.zero, false, false);
+		}
+	}
 }
 
